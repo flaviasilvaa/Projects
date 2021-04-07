febbdc11dd2e4d9b09fad32fab09b1bc7da57fcc
flaviasilvaa <flaviasilvaa@gmail.com>
2021-04-07 22:06:38 +0100

    CalculatingSquareDobleTriple.py

---
+++ CalculatingSquareDobleTriple.py
@@ -0,0 +1,11 @@
+number = int(input("type a number\n"))
+double = number * 2
+triple = number * 3
+root_square = number ** (1/2) #where ** its the power of a number
+print(f'The double of the number is {double}\nthe triple is {triple} and square root is {root_square}')
+
+#########################################################################
+###I can solve in another way
+n = int(input("type a number\n"))
+print(f'The double of the number is {n * 2}.')
+print(f'the triple is {n * 3}\n and square root is {n ** (1/2)}')
